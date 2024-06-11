d2a4f90b4c4e672985bf1d699e643ada619dda5f
Dhruv Auti <dhruvda2@illinois.edu>
2024-06-10 19:22:39 -0500

    backend with stores data

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { getStoredPosts, storePosts } = require('./data/posts');
+
+const { getStoredStores, storeStores, getStoredPosts, storePosts } = require('./data/posts');
 
 const app = express();
 
@@ -38,6 +39,30 @@ app.post('/posts', async (req, res) => {
   const updatedPosts = [newPost, ...existingPosts];
   await storePosts(updatedPosts);
   res.status(201).json({ message: 'Stored new post.', post: newPost });
+});
+
+
+app.get('/stores', async (req, res) => {
+  const storedStores = await getStoredStores();
+  res.json({ stores: storedStores });
+});
+
+app.get('/stores/:name', async (req, res) => {
+  const storedStores = await getStoredStores();
+  const store = storedStores.find((store) => store.name === req.params.name);
+  res.json({ store });
+});
+
+app.post('/stores', async (req, res) => {
+  const existingStores = await getStoredStores();
+  const storeData = req.body;
+  const newStore = {
+    ...storeData,
+    name: storeData.name,
+  };
+  const updatedStores = [newStore, ...existingStores];
+  await storeStores(updatedStores);
+  res.status(201).json({ message: 'Stored new store.', store: newStore });
 });
 
 app.listen(8080);
--- backend/data/posts.js
+++ backend/data/posts.js
@@ -1,15 +1,50 @@
+// const fs = require('node:fs/promises');
+
+// async function getStoredPosts() {
+//   const rawFileContent = await fs.readFile('posts.json', { encoding: 'utf-8' });
+//   const data = JSON.parse(rawFileContent);
+//   const storedPosts = data.posts ?? [];
+
+
+//   return storedPosts;
+// }
+
+// function storePosts(posts) {
+//   return fs.writeFile('posts.json', JSON.stringify({ posts: posts || [] }));
+// }
+
+// exports.getStoredPosts = getStoredPosts;
+// exports.storePosts = storePosts;
+
 const fs = require('node:fs/promises');
 
 async function getStoredPosts() {
   const rawFileContent = await fs.readFile('posts.json', { encoding: 'utf-8' });
   const data = JSON.parse(rawFileContent);
   const storedPosts = data.posts ?? [];
+  
   return storedPosts;
+}
+
+async function getStoredStores() {
+  const rawFileContent = await fs.readFile('stores.json', { encoding: 'utf-8' });
+  const data = JSON.parse(rawFileContent);
+  const storedStores = data.stores ?? [];
+
+  return storedStores;
 }
 
 function storePosts(posts) {
   return fs.writeFile('posts.json', JSON.stringify({ posts: posts || [] }));
 }
 
+function storeStores(stores) {
+  return fs.writeFile('stores.json', JSON.stringify({ stores: stores || [] }));
+}
+
 exports.getStoredPosts = getStoredPosts;
 exports.storePosts = storePosts;
+
+exports.getStoredStores = getStoredStores;
+exports.storeStores = storeStores;
+
